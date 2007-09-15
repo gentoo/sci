7acc521b2b0f3d64944b117fd88313bad75b745d
bicatali <bicatali@32389bae-6d03-0410-99cf-db05cde120eb>
2007-09-15 13:44:13 +0000

    removed cmt, now in main tree
    git-svn-id: http://overlays.gentoo.org/svn/proj/science/overlay@734 32389bae-6d03-0410-99cf-db05cde120eb

--- dev-util/cmt/files/80cmt-mode-gentoo.el
+++
@@ -1,6 +0,0 @@
-;; Automatically load the CMT mode.
-
-(autoload 'cmt-mode "cmt-mode" "CMT requirements file editing mode." t)
-(setq auto-mode-alist 
-      (append (list (cons "requirements$" 'cmt-mode)) auto-mode-alist))
-
