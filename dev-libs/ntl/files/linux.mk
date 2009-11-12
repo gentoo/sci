5927cf4cfabd8edf762e9b396c0532f725a657d7
Sebastien Fabbro <seb@cayenne.ist.utl.pt>
2009-11-12 19:19:42 +0000

    removed ntl, in main tree

--- dev-libs/ntl/files/linux.mk
+++
@@ -1,3 +0,0 @@
-shared: DIRNAME $(SHOBJ)
-	$(LINK_CXX) $(PICFLAG) -shared -Wl,-soname,lib`cat DIRNAME`.so -o lib`cat DIRNAME`.so $(SHOBJ) $(GMP_LIBDIR) $(GMP_LIB)
-	ln -s lib`cat DIRNAME`.so libntl.so
