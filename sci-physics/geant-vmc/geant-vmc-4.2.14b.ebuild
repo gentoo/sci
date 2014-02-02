gentoo/sci

commit 4965054a93b9dd9f630a31b290e20c799dc1cf94

sci-physics/geant-vmc: Add missing die
Package-Manager: portage-2.2.8

sci-physics/geant-vmc/geant-vmc-4.2.14b.ebuild
@@ -34,23 +34,24 @@ src_compile() {
 	local d
 	source $(ls -1 "${EROOT}"usr/share/Geant4-*/geant4make/geant4make.sh) || die
 	for d in ${dirs}; do
-		pushd ${d} > /dev/null
+		pushd ${d} > /dev/null || die
 		emake
-		use doc && doxygen
+		if use doc; then
+			doxygen || die
+		fi
 		popd > /dev/null
 	done
 }
 
 src_test() {
-	cd examples
+	cd examples || die
 	emake
 	./run_suite.sh || die
 }
 
 src_install() {
 	dolib.so lib/tgt_*/{libg4root,libgeant4vmc}.so
-	insinto /usr
-	doins -r include
+	doheader include/*
 	dodoc README history version_number
 	use doc && dohtml -r Geant4VMC.html doc/*
 	if use examples; then

sci-physics/geant-vmc/geant-vmc-4.2.15.ebuild
@@ -33,23 +33,24 @@ src_compile() {
 	local d
 	source $(ls -1 "${EROOT}"usr/share/Geant4-*/geant4make/geant4make.sh) || die
 	for d in ${dirs}; do
-		pushd ${d} > /dev/null
+		pushd ${d} > /dev/null || die
 		emake
-		use doc && doxygen
+		if use doc; then
+			doxygen || die
+		fi
 		popd > /dev/null
 	done
 }
 
 src_test() {
-	cd examples
+	cd examples || die
 	emake
 	./run_suite.sh || die
 }
 
 src_install() {
 	dolib.so lib/tgt_*/{libg4root,libgeant4vmc}.so
-	insinto /usr
-	doins -r include
+	doheader include/*
 	dodoc README history version_number
 	use doc && dohtml -r Geant4VMC.html doc/*
 	if use examples; then

sci-physics/geant-vmc/metadata.xml
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "http://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-<herd>sci-physics</herd>
-<longdescription lang="en">
+  <herd>sci-physics</herd>
+  <longdescription lang="en">
   The Virtual Monte Carlo (VMC) allows to run different simulation
   Monte Carlo without changing the user code and therefore the input
   and output format as well as the geometry and detector response
@@ -13,7 +13,7 @@
   Geant3 VMC (C++) is provided within a single package together with
   GEANT3 (Fortran) - geant3.
 </longdescription>
-<use>
-  <flag name="vgm">Enable the Virtual Geometry Model (<pkg>sci-physics/vgm</pkg>)</flag>
-</use>
+  <use>
+    <flag name="vgm">Enable the Virtual Geometry Model (<pkg>sci-physics/vgm</pkg>)</flag>
+  </use>
 </pkgmetadata>

commit e73ec8e364662db23944ef9d059f44a80a63b225

Make header installation work again, include/ contains folders only so doheader must recurse (got broken in 4965054a93b9dd9f630a31b290e20c799dc1cf94 )

sci-physics/geant-vmc/geant-vmc-4.2.14b.ebuild
@@ -51,7 +51,7 @@ src_test() {
 
 src_install() {
 	dolib.so lib/tgt_*/{libg4root,libgeant4vmc}.so
-	doheader include/*
+	doheader -r include/*
 	dodoc README history version_number
 	use doc && dohtml -r Geant4VMC.html doc/*
 	if use examples; then

sci-physics/geant-vmc/geant-vmc-4.2.15.ebuild
@@ -50,7 +50,7 @@ src_test() {
 
 src_install() {
 	dolib.so lib/tgt_*/{libg4root,libgeant4vmc}.so
-	doheader include/*
+	doheader -r include/*
 	dodoc README history version_number
 	use doc && dohtml -r Geant4VMC.html doc/*
 	if use examples; then