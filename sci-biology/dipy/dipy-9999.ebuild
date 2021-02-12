gentoo/sci

commit c9419e0ec2cbc178aa1df6cfe599808fec40d1c8

*/*: drop obsolete python_targets_python3_6
Hopefully got them all:
* find . | grep -i .ebuild | xargs perl -p -i -e "s/6\.\.9/7\.\.9/"
* find . | grep -i .ebuild | xargs perl -p -i -e "s/3_{6,7/3_{7/"

Signed-off-by: Sam James <sam@gentoo.org>

sci-biology/dipy/dipy-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{6,7} )
+PYTHON_COMPAT=( python3_{7} )
 
 inherit distutils-r1 git-r3
 

commit 2ed0af55538aa194cd07ee71819558afad19b243

sci-biology/dipy: fix syntax
Fixes: c9419e0ec2cbc178aa1df6cfe599808fec40d1c8
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

sci-biology/dipy/dipy-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{7} )
+PYTHON_COMPAT=( python3_7 )
 
 inherit distutils-r1 git-r3
 