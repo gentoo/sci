gentoo/sci

commit c9419e0ec2cbc178aa1df6cfe599808fec40d1c8

*/*: drop obsolete python_targets_python3_6
Hopefully got them all:
* find . | grep -i .ebuild | xargs perl -p -i -e "s/6\.\.9/7\.\.9/"
* find . | grep -i .ebuild | xargs perl -p -i -e "s/3_{6,7/3_{7/"

Signed-off-by: Sam James <sam@gentoo.org>

sci-biology/nilearn/nilearn-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{6,7} )
+PYTHON_COMPAT=( python3_{7} )
 
 inherit distutils-r1 git-r3
 

commit 49dc908a2f1b9c34eb90d07320bac5ac80963e54

sci-biology/nilearn: fix syntax
Fixes: c9419e0ec2cbc178aa1df6cfe599808fec40d1c8
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

sci-biology/nilearn/nilearn-9999.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{7} )
+PYTHON_COMPAT=( python3_7 )
 
 inherit distutils-r1 git-r3
 